e4db9239ac644752b545442ae5680b0cde4e6dc6
chadahun <cdhqw@naver.com>
2023-09-20 18:08:19 +0900

    0920

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
+/*eslint-disable*/
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post ='강남 우동 맛집';  
+  let [글제목,글제목변경]=useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
+  let [하트,하트변경]=useState(0);
+
+  function 함수(){
+    document.querySelector('span').addEventListener('click',function(){
+
+    })
+  }
+
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color:'red',fontSize:'60px'}}>블로그</h4>
+        <h4 style={{color:'red',fontSize:'20px'}}>React Blog</h4>
+        <button onClick={()=>{
+          let copy2=[...글제목];
+          copy2.sort();
+          글제목변경(copy2);
+        }}>가나다순 정렬</button>
+        <button onClick={()=>{let copy=[...글제목];
+        copy[0]='여자 코트 추천';
+        글제목변경(copy);
+      }}>변경</button>
+      </div>
+      <div className="list">
+        <h4>{글제목[0]}<span onClick={()=>{하트변경(하트+1)}}>🤞</span> {하트} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
       </div>
       <h4>{post}</h4>
     </div>
