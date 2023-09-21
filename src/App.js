a3a58aafbed8dd23a302e1805609be2427b737c9
chadahun <cdhqw@naver.com>
2023-09-21 21:15:11 +0900

    0921

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ function App() {
   let post ='강남 우동 맛집';  
   let [글제목,글제목변경]=useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
   let [하트,하트변경]=useState(0);
-
-  function 함수(){
-    document.querySelector('span').addEventListener('click',function(){
-
-    })
-  }
-
+  let [modal,setModal]=useState(false);
 
   return (
     <div className="App">
@@ -39,12 +33,29 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{
+          setModal(!modal);
+          
+        }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
-      <h4>{post}</h4>
+      {
+        modal==true?<Modal></Modal>:null
+         }
     </div>
   );
 }
+
+function Modal(){
+  return(
+    <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+      
+  )
+}
+
 
 export default App;
